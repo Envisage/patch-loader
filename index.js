aeb1bb0d898173db34cd55e653e4cd8ed61702de
Josh Kropf <josh@slashdev.ca>
2017-12-19 23:30:30 -0500

    use less es6

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import fs from 'fs';
-import path from 'path';
-import { parsePatch, applyPatch } from 'diff';
-import loaderUtils from 'loader-utils';
+const fs = require('fs');
+const path = require('path');
+const JsDiff = require('diff');
+const loaderUtils = require('loader-utils');
 
-export default function loader(content) {
+module.exports = function loader(content) {
    const options = loaderUtils.getOptions(this) || {};
 
    if (!options.patch) {
@@ -14,15 +14,15 @@ export default function loader(content) {
    this.addDependency(patchPath);
 
    const patchData = fs.readFileSync(patchPath, 'utf8');
-   const patch = parsePatch(patchData);
+   const patch = JsDiff.parsePatch(patchData);
 
    if (Array.isArray(patch) && patch.length > 0 && patch[0].hunks.length > 0) {
-      const result = applyPatch(content, patch);
+      const result = JsDiff.applyPatch(content, patch);
       if (!result) {
          throw new Error('Patch could not apply cleanly');
       }
 
-      return `export default ${result}`;
+      return `module.exports = ${result}`;
    } else {
       throw new Error('Invalid patch file format');
    }
